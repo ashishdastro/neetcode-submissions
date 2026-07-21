98332f078bb55a36ffe2e5ab3cc6897cc52ac979
ashish mishra <ashish.mishra231@gmail.com>
2026-07-20 23:26:09 -0500

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        prev, cur = 1, 2
+
+        for _ in range(3, n+1):
+            prev, cur = cur, prev + cur
+        
+        return cur
