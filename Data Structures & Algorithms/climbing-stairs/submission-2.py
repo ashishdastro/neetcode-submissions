14d745795404558e5624c21a99df21248eda027f
ashish mishra <ashish.mishra231@gmail.com>
2026-07-20 23:18:38 -0500

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:   
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        def dfs(n):
+            if n  <= 2:
+                return n
+            
+            if n not in memo:
+                memo[n] = dfs(n-1) + dfs(n-2)
+
+            return memo[n]
+        
+        return dfs(n)
