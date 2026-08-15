28c0bb49a7c984a7e259b8ec01590ae163df7a34
ashish mishra <ashish.mishra231@gmail.com>
2026-08-15 12:11:51 -0500

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c):
+            if (
+                not 0 <= r < rows or
+                not 0 <= c < cols or
+                board[r][c] != "O"
+            ):
+                return 
+            
+            board[r][c] = "T"
+
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        
+        # 1. Find O's connected to left/right borders
+        for r in range(rows):
+            if board[r][0] == "O":
+                dfs(r, 0)
+            if board[r][cols - 1] == "O":
+                dfs(r, cols - 1)
+        
+        # 2. Find O's connected to top/bottom borders
+        for c in range(cols):
+            if board[0][c] == "O":
+                dfs(0, c)
+            if board[rows - 1][c] == "O":
+                dfs(rows - 1, c)
+
+        # 3. Capture surrounded regions
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
